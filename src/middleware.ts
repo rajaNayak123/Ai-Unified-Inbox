e9bc1c6e33249b1abfe14970bbd618b6278f2dcd
rajaNayak123 <nayakraja151@gmail.com>
2026-05-25 17:36:49 +0530

    Added middleware remove proxy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-export { default as proxy } from 'next-auth/middleware'
+export { default } from 'next-auth/middleware'
 
 export const config = {
   // Protect these routes — unauthenticated users are redirected to /login
